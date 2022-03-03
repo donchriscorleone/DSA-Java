cc2aee403f86be7882a3431bcdc5b796a4859b31
Christopher II Lajom <christopheriilajom@gmail.com>
2022-03-03 20:49:55 +0800

    Added reverse queue method.

--- DataStructuresAndAlgorithms.java
+++ DataStructuresAndAlgorithms.java
@@ -6,6 +6,9 @@
 package datastructuresandalgorithms;
 
 import datastructuresandalgorithms.Stacks.*;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Stack;
 
 /**
  *
@@ -17,14 +20,41 @@ public class DataStructuresAndAlgorithms {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Stack stack = new Stack();
-        stack.push(1);
-        stack.push(2);
-        stack.push(3);
-
-        System.out.println(stack.peek());
-        System.out.println(stack.pop());
-        System.out.println(stack);
+        // first we declare a queue class
+        Queue<Integer> queue = new ArrayDeque<>();
+        
+        // add items to our queue
+        queue.add(10);
+        queue.add(20);
+        queue.add(30);
+        
+        // print the initial items
+        System.out.println(queue);
+        
+        // use the reverse method
+        reverse(queue);
+        
+        // remove the first item in the queue
+        System.out.println("First item " + queue.remove());
+        // print again the queue
+        System.out.println(queue);
     }
     
+    // create the reverse method
+    public static void reverse(Queue<Integer> queue) {
+        // We will use stacks for reversing
+        Stack<Integer> stack = new Stack<>();
+        
+        // iterate the queue
+        while (!queue.isEmpty()) // while the items in queue are not empty
+            stack.push(queue.remove());
+        
+        // iterate the stack
+        while (!stack.isEmpty())
+            queue.add(stack.pop());
+        
+        // print the queue
+        System.out.println(queue);
+    }
+        
 }
